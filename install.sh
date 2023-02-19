81365685200bd5f1ba693669f5d1a326c40ddb49
Ben Amies-King <bamiesking@users.noreply.github.com>
2023-02-19 22:34:29 +0000

    Added initial install.sh
    - Installs Neovim and sets up config

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo git clone https://github.com/neovim/neovim ~/nvim-install
+(cd ~/nvim-install ; sudo make CMAKE_BUILD_TYPE=RelWithDebInfo ; sudo make install)
+sudo mkdir ~/.config/nvim
+sudo git clone https://bamiesking/nvim-config ~/.config/nvim
+sudo apt-get install ripgrep fd-find --yes 
+sudo apt-get autoremove --yes
